736b9bff9d8fac03241b4d2d4d6740cfd460293d
Jake Wengroff <JakeWengroff@Jakes-MacBook-Air.local>
2016-06-12 21:53:48 -0400

    Added getInitialState.

--- public/scripts/tutorial1.js
+++ public/scripts/tutorial1.js
@@ -1,10 +1,13 @@
 var CommentBox = React.createClass({
+  getInitialState: function() {
+    return {data: []};
+  },
   render: function() {
     return (
       <div className="commentBox">
         Hello, world! I am a CommentBox.
         <h1>Comments</h1>
-        <CommentList data={this.props.data} />
+        <CommentList data={this.state.data} />
         <CommentForm />
       </div>
     );
