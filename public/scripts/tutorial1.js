307984137adf4cbc71ccdb1cd13d3cafcc2a4120
Jake Wengroff <JakeWengroff@Jakes-MacBook-Air.local>
2016-06-12 21:34:51 -0400

    Added a Data model.

--- public/scripts/tutorial1.js
+++ public/scripts/tutorial1.js
@@ -4,7 +4,7 @@ var CommentBox = React.createClass({
       <div className="commentBox">
         Hello, world! I am a CommentBox.
         <h1>Comments</h1>
-        <CommentList />
+        <CommentList data={this.props.data} />
         <CommentForm />
       </div>
     );
@@ -13,15 +13,20 @@ var CommentBox = React.createClass({
 
 var CommentList = React.createClass({
   render: function() {
+    var commentNodes = this.props.data.map(function(comment) {
+      return (
+        <Comment author={comment.author} key={comment.id}>
+          {comment.text}
+        </Comment>
+      );
+    });
     return (
       <div className="commentList">
-        <Comment author="Pete Hunt">This is one comment.</Comment>
-        <Comment author="Jordan Walke">This is *another* comment.</Comment>
-        Hello, world! I am a CommentList.
+        {commentNodes}
       </div>
     );
   }
-});
+});  
 
 var CommentForm = React.createClass({
   render: function() {
@@ -52,7 +57,12 @@ var Comment = React.createClass({
   }
 });
 
+var data = [
+  {id: 1, author: "Pete Hunt", text: "This is one comment."},
+  {id: 2, author: "Jordan Walke", text: "This is *another* comment."}
+];
+
 ReactDOM.render(
-  <CommentBox />,
+  <CommentBox data={data} />,
   document.getElementById('content')
 );
